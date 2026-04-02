bf58b5c98b4ae405bee258817728d162aa1a3cec
stefangamperl-stack <stefan.gamperl@gmail.com>
2026-04-02 13:25:52 +0200

    Initialize Supabase client in supabaseClient.js

---
+++ js/supabaseClient.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = "https://qxrrhwkaesubexqmfhks.supabase.co";
+const SUPABASE_KEY = "sb_publishable_qYO5Ox2yDKfJHqrlFg1iCg_cha-jVZ0";
+
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
